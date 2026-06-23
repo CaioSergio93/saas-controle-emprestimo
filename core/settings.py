e2a93136bc7192836adeedb8ae89324aca6dd568
Caio Sergio <89256972+CaioSergio93@users.noreply.github.com>
2026-06-23 11:57:08 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,12 @@ SECRET_KEY = 'django-insecure-_t8e=#pj^@bfi^5e^l7s&li5gnevrts!+h=a-ke&n1o6adk2c5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['saas-controle-emprestimo.onrender.com']
+ALLOWED_HOSTS = [
+    'saas-controle-emprestimo.onrender.com',
+    '.onrender.com',
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
